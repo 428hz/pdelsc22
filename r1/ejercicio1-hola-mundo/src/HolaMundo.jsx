f1c150846e020ee8bb71faf116c43a19e27dbb7e
428hz <holasoyian.o@gmail.com>
2025-08-13 18:23:32 -0300

    pdelsc22

--- r1/ejercicio1-hola-mundo/src/HolaMundo.jsx
+++ r1/ejercicio1-hola-mundo/src/HolaMundo.jsx
@@ -1,12 +1,13 @@
-import React from 'react';//importa el react
-import HolaMundo from './HolaMundo.jsx';// importa el el codigo 
-
-function App() {
+import react from 'react';
+import './HolaMundo.css'; // acá traemos los estilos que creaste
+import './App.css'; 
+function holamundo() {
+  // y acá usamos la clase 'saludo' para que se vea lindo
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <HolaMundo />
+    <div className="saludo">
+      <h1>¡hola, mundo!</h1>
     </div>
-  );//alineacion
+  );
 }
 
-export default App;
+export default holamundo;
