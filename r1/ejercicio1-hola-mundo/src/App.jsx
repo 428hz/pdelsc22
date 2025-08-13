b70c1dcdc33eb3b3e3f4992fac78872b7c8505a1
428hz <holasoyian.o@gmail.com>
2025-08-13 17:38:51 -0300

    fdsdfs

--- r1/ejercicio1-hola-mundo/src/App.jsx
+++ r1/ejercicio1-hola-mundo/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import HolaMundo from './HolaMundo.jsx'; // Importa el componente que creaste
+import React from 'react'; //importa el react
+import HolaMundo from './HolaMundo.jsx'; // importa el el codigo 
 
 function App() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <HolaMundo />
+      <HolaMundo /> 
     </div>
-  );
+  );// toda la alineacion
 }
 
 export default App;
--- r1/ejercicio1-hola-mundo/src/HolaMundo.jsx
+++ r1/ejercicio1-hola-mundo/src/HolaMundo.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import HolaMundo from './HolaMundo.jsx';
+import React from 'react';//importa el react
+import HolaMundo from './HolaMundo.jsx';// importa el el codigo 
 
 function App() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <HolaMundo />
     </div>
-  );
+  );//alineacion
 }
 
 export default App;
